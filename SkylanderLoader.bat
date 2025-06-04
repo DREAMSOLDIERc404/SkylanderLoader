c42e29b76226a561cf5be224225949a9d17cc796
Lorenzo Boccuzzi <lorenzo.boccuzzi@studio.unibo.it>
2025-06-04 13:51:26 +0200

    Migliorato il launcher

--- SkylanderLoader.bat
+++ SkylanderLoader.bat
@@ -1,6 +1,8 @@
 @echo off
+title SkylanderLoader
+mode con: cols=70 lines=40
 cd %~dp0
 call setup\setup.cmd
-call msys2\msys2_shell.cmd -mingw64 -defterm -no-start %* ./SkylanderLoader
+call msys2\msys2_shell.cmd -mingw64 -defterm -no-start %* "./SkylanderLoader"
 if not exist setup\installed64.txt msys2\msys2_shell.cmd -mingw64 -defterm -no-start %*
-
+exit
